a82d8aabbda01d2839e3861bcfe173b35373e371
Sw3d15h-F1s4 <samjt36@gmail.com>
2023-09-03 12:27:55 -0400

    add binding for telescope diagnostics

--- nvchad/.config/nvim/lua/core/mappings.lua
+++ nvchad/.config/nvim/lua/core/mappings.lua
@@ -284,6 +284,7 @@ M.telescope = {
         ["<leader>fh"] = { "<cmd> Telescope help_tags <CR>", "Help page" },
         ["<leader>fo"] = { "<cmd> Telescope oldfiles <CR>", "Find oldfiles" },
         ["<leader>fz"] = { "<cmd> Telescope current_buffer_fuzzy_find <CR>", "Find in current buffer" },
+        ["<leader>fd"] = { "<cmd> Telescope diagnostics <CR>", "Show all diagnostics" },
 
         -- git
         ["<leader>gc"] = { "<cmd> Telescope git_commits <CR>", "Git commits" },
