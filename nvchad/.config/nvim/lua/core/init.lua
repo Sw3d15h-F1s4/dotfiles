7941bcbed9a2f67ed9a6ad4daff469952379340a
Sw3d15h-F1s4 <samjt36@gmail.com>
2023-09-03 12:23:40 -0400

    becoming chad of nvchad
    this is beyond chad, this is sigmanv!!!
    i dont want nvchad updates so i just removed the custom directory and manually edit everything.
    im only doing this cuz telescope looks nice. thats about it.

--- nvchad/.config/nvim/lua/core/init.lua
+++ nvchad/.config/nvim/lua/core/init.lua
@@ -1,7 +1,6 @@
 local opt = vim.opt
 local g = vim.g
 local config = require("core.utils").load_config()
-
 -------------------------------------- globals -----------------------------------------
 g.nvchad_theme = config.ui.theme
 g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
@@ -12,15 +11,15 @@ g.transparency = config.ui.transparency
 opt.laststatus = 3 -- global statusline
 opt.showmode = false
 
-opt.clipboard = "unnamedplus"
+opt.clipboard = "unnamed"
 opt.cursorline = true
 
 -- Indenting
 opt.expandtab = true
-opt.shiftwidth = 2
+opt.shiftwidth = 4
 opt.smartindent = true
-opt.tabstop = 2
-opt.softtabstop = 2
+opt.tabstop = 4
+opt.softtabstop = 4
 
 opt.fillchars = { eob = " " }
 opt.ignorecase = true
@@ -49,11 +48,15 @@ opt.updatetime = 250
 -- when cursor reaches end/beginning of line
 opt.whichwrap:append "<>[]hl"
 
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.wrap = false
+
 g.mapleader = " "
 
 -- disable some default providers
 for _, provider in ipairs { "node", "perl", "python3", "ruby" } do
-  vim.g["loaded_" .. provider .. "_provider"] = 0
+    vim.g["loaded_" .. provider .. "_provider"] = 0
 end
 
 -- add binaries installed by mason.nvim to path
@@ -65,51 +68,44 @@ local autocmd = vim.api.nvim_create_autocmd
 
 -- dont list quickfix buffers
 autocmd("FileType", {
-  pattern = "qf",
-  callback = function()
-    vim.opt_local.buflisted = false
-  end,
+    pattern = "qf",
+    callback = function()
+        vim.opt_local.buflisted = false
+    end,
 })
 
 -- reload some chadrc options on-save
 autocmd("BufWritePost", {
-  pattern = vim.tbl_map(function(path)
-    return vim.fs.normalize(vim.loop.fs_realpath(path))
-  end, vim.fn.glob(vim.fn.stdpath "config" .. "/lua/custom/**/*.lua", true, true, true)),
-  group = vim.api.nvim_create_augroup("ReloadNvChad", {}),
+    pattern = vim.tbl_map(function(path)
+        return vim.fs.normalize(vim.loop.fs_realpath(path))
+    end, vim.fn.glob(vim.fn.stdpath "config" .. "/lua/custom/**/*.lua", true, true, true)),
+    group = vim.api.nvim_create_augroup("ReloadNvChad", {}),
 
-  callback = function(opts)
-    local fp = vim.fn.fnamemodify(vim.fs.normalize(vim.api.nvim_buf_get_name(opts.buf)), ":r") --[[@as string]]
-    local app_name = vim.env.NVIM_APPNAME and vim.env.NVIM_APPNAME or "nvim"
-    local module = string.gsub(fp, "^.*/" .. app_name .. "/lua/", ""):gsub("/", ".")
+    callback = function(opts)
+        local fp = vim.fn.fnamemodify(vim.fs.normalize(vim.api.nvim_buf_get_name(opts.buf)), ":r") --[[@as string]]
+        local app_name = vim.env.NVIM_APPNAME and vim.env.NVIM_APPNAME or "nvim"
+        local module = string.gsub(fp, "^.*/" .. app_name .. "/lua/", ""):gsub("/", ".")
 
-    require("plenary.reload").reload_module "base46"
-    require("plenary.reload").reload_module(module)
-    require("plenary.reload").reload_module "custom.chadrc"
+        require("plenary.reload").reload_module "base46"
+        require("plenary.reload").reload_module(module)
 
-    config = require("core.utils").load_config()
+        config = require("core.utils").load_config()
 
-    vim.g.nvchad_theme = config.ui.theme
-    vim.g.transparency = config.ui.transparency
+        vim.g.nvchad_theme = config.ui.theme
+        vim.g.transparency = config.ui.transparency
 
-    -- statusline
-    require("plenary.reload").reload_module("nvchad.statusline." .. config.ui.statusline.theme)
-    vim.opt.statusline = "%!v:lua.require('nvchad.statusline." .. config.ui.statusline.theme .. "').run()"
+        -- statusline
+        require("plenary.reload").reload_module("nvchad.statusline." .. config.ui.statusline.theme)
+        vim.opt.statusline = "%!v:lua.require('nvchad.statusline." .. config.ui.statusline.theme .. "').run()"
 
-    -- tabufline
-    if config.ui.tabufline.enabled then
-      require("plenary.reload").reload_module "nvchad.tabufline.modules"
-      vim.opt.tabline = "%!v:lua.require('nvchad.tabufline.modules').run()"
-    end
+        -- tabufline
+        if config.ui.tabufline.enabled then
+            require("plenary.reload").reload_module "nvchad.tabufline.modules"
+            vim.opt.tabline = "%!v:lua.require('nvchad.tabufline.modules').run()"
+        end
 
-    require("base46").load_all_highlights()
-    -- vim.cmd("redraw!")
-  end,
+        require("base46").load_all_highlights()
+        -- vim.cmd("redraw!")
+    end,
 })
 
--------------------------------------- commands ------------------------------------------
-local new_cmd = vim.api.nvim_create_user_command
-
-new_cmd("NvChadUpdate", function()
-  require "nvchad.updater"()
-end, {})
